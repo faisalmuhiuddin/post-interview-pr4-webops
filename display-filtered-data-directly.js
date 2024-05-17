f0bc139fa8ee1451d5a9d9ede6e5e63ea551b9a3
Faisal Muhiuddin <136606230+faisalmuhiuddin@users.noreply.github.com>
2024-05-17 10:35:20 +0530

    Create display-filtered-data-directly.js

---
+++ display-filtered-data-directly.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/api/data', (req, res) => {
+    fs.readFile("mess.json", "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+
+        let jsonData = JSON.parse(data);
+        const { type, week, day, meal } = req.query;
+
+         if (type) {
+            jsonData = jsonData[type];
+            if (week) {
+                jsonData = jsonData?.[week];
+                if (day) {
+                    jsonData = jsonData?.[day];
+                    if (meal) {
+                        jsonData = jsonData?.[meal];
+                    }
+                }
+            }
+        }
+//instead of directly displaying data using res.JSON, I have added a HTML rendering part that will display data
+        if (!jsonData) {
+            res.send("<html><body>No data found</body></html>");
+        } else {
+            const displayData = JSON.stringify(jsonData);
+            res.send(`<html><body><h1>Data display: </h1>${displayData}</body></html>`);
+
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 3000");
+})
+
+// http://localhost:3000/api/data?type=South%20Veg&week=odd&day=Sunday&meal=Breakfast
